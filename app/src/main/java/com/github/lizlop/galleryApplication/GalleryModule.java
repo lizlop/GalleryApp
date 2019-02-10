dae697a03c32ec5251237b4d20bd9894e613e4a0
lizlop <liza.lopyreva@gmail.com>
2019-02-11 00:41:13 +0300

    savepoint

---
+++ app/src/main/java/com/github/lizlop/galleryApplication/GalleryModule.java
@@ -0,0 +1,10 @@
+package com.github.lizlop.galleryApplication;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+
+@GlideModule
+public final class GalleryModule extends AppGlideModule {
+
+}
